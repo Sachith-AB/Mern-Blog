0431b2561e9f81eefa8a7dfed21dbca588c159ff
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-05-08 09:09:42 +0530

    show the comment of that post

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import { verifyToken } from '../utiles/varifyUser.js';
 import { createComment } from '../controllers/comment.controller.js';
+import { getPostComments } from '../controllers/comment.controller.js';
 
 const router = express.Router();
 
 router.post('/create',verifyToken,createComment)
+router.get('/getPostComments/:postId',getPostComments)
 
 export default router;
